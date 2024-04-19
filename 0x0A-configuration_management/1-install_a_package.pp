2d5274d201a780a10c4d78937513d77f8b83f094
Ambogo2 <ruthambogo.ra@gmail.com>
2024-04-19 22:03:57 +0300

    create package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,12 @@
 # contains manifest to install a package
 
     package { 'flask':
-        ensure    => '2.1.0',
-        provider  => 'pip3'
+        ensure   => '2.1.0',
+        provider => 'pip3'
 
 }
     package { 'werkzeug':
-        ensure    => '2.1.1',
-        provider  => 'pip3'
+        ensure   => '2.1.1',
+        provider => 'pip3'
 
 }
